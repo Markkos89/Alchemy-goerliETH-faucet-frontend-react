63de25cf8a4bb3f67d59b36290163666c7789109
Marcos Iglesias <markkos89@gmail.com>
2022-11-20 04:00:29 -0300

    feat: small refactor and set basic config values

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <nav>
+        <div className="navbar-header">
+          <h1>Alchemy University</h1>
+        </div>
+      </nav>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
-import './index.css';
-import App from './App.jsx';
-import ReactDOM from 'react-dom/client';
-import Layout from './layout';
-import Navbar from './navbar';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
-import { WagmiConfig, createClient } from 'wagmi';
-import { getDefaultProvider } from 'ethers';
+import "./index.css";
+import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import React from "react";
 
-const client = createClient({
-  autoConnect: true,
-  provider: getDefaultProvider(),
-})
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // Path direct us to the right page
 root.render(
-  <WagmiConfig client={client}>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-      <Route index element={<App />} />
-      <Route path="*" element={<App />} />
-      </Route>
-    </Routes>
-    </BrowserRouter> 
-  </WagmiConfig>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 reportWebVitals();
